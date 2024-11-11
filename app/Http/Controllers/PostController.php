d9551a37d8b631013523bacb721affde06760045
pipisa <your@email.ex>
2024-11-11 10:42:14 +0200

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
-        return view('posts.index');
+        return view('posts.index', ['allPosts' => $posts]);
     }
 
     /**
