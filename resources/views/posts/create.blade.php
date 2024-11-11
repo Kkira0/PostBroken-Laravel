3bef9361008eb44f333aa25236ebb9196f5a6a2a
pipisa <your@email.ex>
2024-11-11 10:59:14 +0200

    salabots

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,6 +9,10 @@
         <label for="content">Content: </label>
         <textarea name="content" id="content"></textarea>
         <br>
+        
+        <label for="published_at">Published at: </label>
+        <input type="text" id="published_at" name="published_at">
+        <br>
         <input type="submit" value="Create">
     </form>
 </x-app-layout>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,14 +1,17 @@
 <x-app-layout>
     <h1>Edit post</h1>
 
-    <form action="" method="post">
+    <form action="/posts/{{$post->id}}/update" method="post">
         @csrf
-
+        @method('PUT')
         <label for="title">Title: </label>
         <input type="text" id="title" name="title" value="{{ $post->title }}">
         <br>
         <label for="content">Content: </label>
         <textarea name="content" id="content">{{ $post->content }}</textarea>
+        <br>
+        <label for="published_at">Published at: </label>
+        <input type="text" id="published_at" name="published_at" value="{{ $post->published_at }}">
         <br>
         <input type="submit" value="Update">
     </form>
